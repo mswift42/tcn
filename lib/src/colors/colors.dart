2a2816e2965b0c9b012cedee50d4d8bc2b527876
mswift42 <martin.haesler@gmail.com>
2022-04-02 10:09:39 +0100

    add colors.

---
+++ lib/src/colors/colors.dart
@@ -0,0 +1,24 @@
+
+import 'package:material_color_utilities/material_color_utilities.dart';
+
+class Colors {
+
+  var MainBgColor = HctColor.fromInt(0xffffffff);
+  var MainFgColor = HctColor.fromInt(0xff000000);
+  var KeywordColro = HctColor.fromInt(0xff000000);
+  var FuncNameColor = HctColor.fromInt(0xff000000);
+  var BuiltinColor  = HctColor.fromInt(0xff000000);
+  var StringColor = HctColor.fromInt(0xff000000);
+  var VariableColor = HctColor.fromInt(0xff000000);
+  var TypeColor = HctColor.fromInt(0xff000000);
+  var ConstantColor = HctColor.fromInt(0xff000000);
+  var CommentColor = HctColor.fromInt(0xff000000);
+  var WarningColor = HctColor.fromInt(0xff000000);
+  var WarningColor2 = HctColor.fromInt(0xff000000);
+  var Bg2Color = HctColor.fromInt(0xff000000);
+  var Bg3Color = HctColor.fromInt(0xff000000);
+  var Bg0Color = HctColor.fromInt(0xff000000);
+
+
+
+}
